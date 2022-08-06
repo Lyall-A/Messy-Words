515da0c05bfb5e33876b963c6180cc41ed8e5d1d
Lyall <58636255+lyall-pc@users.noreply.github.com>
2022-08-06 21:58:26 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Type something: ";
+    getline(cin, input);
+    string messy = input;
+    transform(messy.begin(), messy.end(), messy.begin(), ::toupper);
+
+    replace(messy.begin(), messy.end(), 'A', '4');
+    replace(messy.begin(), messy.end(), 'E', '3');
+    replace(messy.begin(), messy.end(), 'I', '1');
+    replace(messy.begin(), messy.end(), 'O', '0');
+    replace(messy.begin(), messy.end(), 'S', '5');
+    replace(messy.begin(), messy.end(), 'T', '7');
+
+    cout << messy;
+    return 0;
+}
